e0aedea3a28c5e7c1c85364b3726aa30be9281c2
Ionela Palacean <ione.caia85@gmail.com>
2019-09-16 19:08:40 +0300

    Solved RetirementCalculator

---
+++ src/main/java/org/fasttrackit/RetirementCalculator.java
@@ -0,0 +1,27 @@
+package org.fasttrackit;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class RetirementCalculator {
+
+    public static void main(String[] args) {
+
+        System.out.println("What is your current age? ");
+        Scanner scanner = new Scanner(System.in);
+        int age=scanner.nextInt();
+
+        System.out.println("At what age would you like to retire? ");
+        int retirementAge=scanner.nextInt();
+
+        LocalDate currentDate=LocalDate.now();
+
+       int remainingyears= retirementAge-age;
+
+        System.out.println("You have"+ remainingyears+" years left until retirement");
+        System.out.println("It's"+currentDate.getYear()+" so you can retire in"+
+                (currentDate.getYear()+remainingyears) );
+
+
+    }
+}
